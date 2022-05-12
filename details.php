74295a2c8cfe8bd3712211cf97f1ad6d59d788c7
Piotr Szczesniak <piotr.szczesniak@trafiony.com>
2022-05-12 13:50:29 +0200

    details.php refactor

--- details.php
+++ details.php
@@ -17,10 +17,7 @@ include ('config/db_connect.php');
 
         mysqli_free_result($results);
         mysqli_close($dbConnection);
-
-
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -29,41 +26,41 @@ include ('config/db_connect.php');
 <?php include('components/header.php'); ?>
 
 <div class="container">
-    <?php if($product) : ;?>
-    <div class="row">
-        <h1 class="h1 pt-3 pb-3 text-center">
-            <?php echo htmlspecialchars($product['title']); ?>
-        </h1>
-    </div>
-    <div class="row d-flex justify-content-center">
-        <div class="col-sm-12 col-md-8 mb-4">
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title fw-bold "><?php echo ucfirst(htmlspecialchars($product['title'])) ;?></h5>
-                    <ul class="list-group list-group-flush">
-                        <?php foreach(explode(',',$product['features']) as $feature) : ;?>
-                        <li class="list-group-item ps-0">
-                            <?php echo ucwords(htmlspecialchars($feature)) ;?>
-                        </li>
-                        <?php endforeach ;?>
-                    </ul>
-                    <p class="fw-bold card-text">Price: <?php echo htmlspecialchars($product['price']) ;?> PLN</p>
-                    <small>
-                        Created at
-                        <?php echo htmlspecialchars($product['created_at']) ;?> by
-                        <?php echo htmlspecialchars($product['email']) ;?>
-                    </small>
-                </div>
-            </div>
+  <?php if($product) : ;?>
+  <div class="row">
+    <h1 class="h1 pt-3 pb-3 text-center">
+      <?php echo htmlspecialchars($product['title']); ?>
+    </h1>
+  </div>
+  <div class="row d-flex justify-content-center">
+    <div class="col-sm-12 col-md-8 mb-4">
+      <div class="card shadow-sm">
+        <div class="card-body">
+          <h5 class="card-title fw-bold "><?php echo ucfirst(htmlspecialchars($product['title'])) ;?></h5>
+          <ul class="list-group list-group-flush">
+            <?php foreach(explode(',',$product['features']) as $feature) : ;?>
+            <li class="list-group-item ps-0">
+              <?php echo ucwords(htmlspecialchars($feature)) ;?>
+            </li>
+            <?php endforeach ;?>
+          </ul>
+          <p class="fw-bold card-text">Price: <?php echo htmlspecialchars($product['price']) ;?> PLN</p>
+          <small>
+            Created at
+            <?php echo htmlspecialchars($product['created_at']) ;?> by
+            <?php echo htmlspecialchars($product['email']) ;?>
+          </small>
         </div>
+      </div>
     </div>
-    <?php else : ;?>
-    <div class="row">
-        <h1 class="h1 pt-3 pb-3 text-center">
-            No such product..., sorry! Add new product!
-        </h1>
-    </div>
-    <?php endif ;?>
+  </div>
+  <?php else : ;?>
+  <div class="row">
+    <h1 class="h1 pt-3 pb-3 text-center">
+      No such product..., sorry! Add new product!
+    </h1>
+  </div>
+  <?php endif ;?>
 </div>
 
 <?php include('components/footer.php') ?>
